f4baf8c92eb3f7c336c8ca657ac062c86f75f49c
Sébastien Cabana <156612899+CABS72030202@users.noreply.github.com>
2025-05-30 11:44:48 -0400

    Final basic layout

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,12 +1,20 @@
-let currentLang = "en";
+let currentLang = "fr";
 
 document.addEventListener("DOMContentLoaded", () => {
   const langToggle = document.getElementById("lang-toggle");
+  const menuToggle = document.querySelector('.menu-toggle');
+  const mainNav = document.querySelector('.main-nav');
 
+  // Language toggle functionality
   langToggle.addEventListener("click", () => {
     currentLang = currentLang === "en" ? "fr" : "en";
     langToggle.textContent = currentLang === "en" ? "FR" : "EN";
     updateLanguage();
+  });
+
+  // Mobile menu toggle functionality
+  menuToggle.addEventListener('click', () => {
+    mainNav.classList.toggle('active');
   });
 
   function updateLanguage() {
@@ -14,4 +22,6 @@ document.addEventListener("DOMContentLoaded", () => {
       el.textContent = el.getAttribute(`data-${currentLang}`);
     });
   }
+
+  updateLanguage(); // set FR initially
 });
